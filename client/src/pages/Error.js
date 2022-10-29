cc25e7aadf50549a08b1a36b3119519078669aaa
coloradobaggins <bugio89@gmail.com>
2022-10-29 12:41:18 -0300

    update error page

--- client/src/pages/Error.js
+++ client/src/pages/Error.js
@@ -1,8 +1,17 @@
+import { Link } from 'react-router-dom';
+import img from '../assets/images/not-found.svg';
+import Wrapper from '../assets/wrappers/ErrorPage';
+
 const Error = ()=>{
     return(
-        <div>
-            <h1>Error</h1>
-        </div>
+        <Wrapper className='full-page'>
+            <div>
+                <img src={img} alt="not found" />
+                <h3>Error</h3>
+                <p>Text!!</p>
+                <Link to='/'>Go Home</Link>
+            </div>
+        </Wrapper>
     )
 }
 
